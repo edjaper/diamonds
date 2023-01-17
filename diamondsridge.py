efd182d3634e5dd6fdf857249b65a92541f4780a
edjaper <66147047+edjaper@users.noreply.github.com>
2023-01-17 19:55:24 -0300

    Update diamondsridge.py

--- diamondsridge.py
+++ diamondsridge.py
@@ -16,7 +16,7 @@ st.write(df.iloc[[id], :])
 #    st.write(diamonds)
 
 texto = feedback.loc[id, 'feedback']
-if (texto.isnan()):
+if (not text):
     texto=""
 txt = st.text_area('Feedback', value=texto)
 
