52f9c002f687c161efad22606dc8476d28242edf
edjaper <66147047+edjaper@users.noreply.github.com>
2023-01-17 19:16:16 -0300

    Update diamondsridge.py

--- diamondsridge.py
+++ diamondsridge.py
@@ -2,40 +2,20 @@ from sklearn.linear_model import Ridge
 import streamlit as st
 import pandas as pd
 import sklearn_json as skljson
-
-#!pip install lime
 import lime
 import lime.lime_tabular
 import numpy as np
 
 
-diamonds = st.cache(pd.read_csv)("diamonds.csv", sep=";", decimal=".")
-feedback = pd.read_csv("feedback.csv", sep=";")
+df = st.cache(pd.read_csv)('df_x_SKBfregression_545noADME_withYandYpredandId.csv', sep=',', decimal='.')
 
-
-if "feedback" not in st.session_state:
-    st.session_state['feedback'] = pd.DataFrame(columns=['id','feedback'])
+#if "feedback" not in st.session_state:
+#    st.session_state['feedback'] = pd.DataFrame(columns=['id','feedback'])
     
-#!pip install sklearn-json
 
-#Loading up the Regression model we created
-model = Ridge()
-model = skljson.from_json('rr_model.json')
-
-
-clarity = st.selectbox( 'Which clarity do you like best?', diamonds['clarity'].unique()) 
-'You selected clarity: ', clarity
-
-
-colors = st.multiselect('What are your favorite colors?', diamonds['color'].unique())
-st.write('You selected colors:', colors)
-st.write('You selected colors:', type((colors))
-
-# Filter dataframe
-#new_df = diamonds[(diamonds['clarity'].isin(clarity)) & (diamonds['color'].isin(colors))]
-new_df = diamonds.copy()
-# write dataframe to screen
-st.write(new_df)
+id = st.selectbox( 'Which clarity do you like best?', df['id'].unique()) 
+'You selected clarity: ', id
+st.write(df[id]
 
 
 #if st.checkbox('Show dataframe'):
