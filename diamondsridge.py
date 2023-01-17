6512231898042864165d0db5427ee39237c1721f
edjaper <66147047+edjaper@users.noreply.github.com>
2023-01-17 20:33:08 -0300

    Update diamondsridge.py

--- diamondsridge.py
+++ diamondsridge.py
@@ -3,10 +3,22 @@ import pandas as pd
 import numpy as np
 
 df = st.cache(pd.read_csv)('df_x_SKBfregression_545noADME_withYandYpredandId.csv', sep=',', decimal='.')
-feedback = st.cache(pd.read_csv)('feedback.csv', sep=';;#,;', decimal='.')
+feedback = st.cache(pd.read_csv, ttl=2)('feedback.csv', sep=';;#,;', decimal='.')
 
 #if "feedback" not in st.session_state:
 #    st.session_state['feedback'] = pd.DataFrame(columns=['id','feedback'])
+
+
+
+def onSave(a, b):
+    data = {
+            'id':a,
+            'feedback':b
+        }
+    st.session_state['feedback'] = st.session_state['feedback'].append(data, ignore_index=True)
+
+
+    
     
 id = st.selectbox( 'Which clarity do you like best?', df['id'].unique()) 
 'You selected clarity: ', id
@@ -23,3 +35,5 @@ if (texto=="nan"):
 
 txt = st.text_area('Feedback', value=texto)
 txt = st.text_input('Feedback', value=texto)
+
+st.button("Salvar", on_click = onSave(id, txt)
