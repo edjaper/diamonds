36b1303ffc5b1e20a868d76d5c8619955c940221
edjaper <66147047+edjaper@users.noreply.github.com>
2023-01-17 19:34:14 -0300

    Update diamondsridge.py

--- diamondsridge.py
+++ diamondsridge.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 
 df = st.cache(pd.read_csv)('df_x_SKBfregression_545noADME_withYandYpredandId.csv', sep=',', decimal='.')
+feedback = st.cache(pd.read_csv)('feedback.csv', sep=';', decimal='.')
 
 #if "feedback" not in st.session_state:
 #    st.session_state['feedback'] = pd.DataFrame(columns=['id','feedback'])
@@ -14,7 +15,8 @@ st.write(df.iloc[[id], :])
 #if st.checkbox('Show dataframe'):
 #    st.write(diamonds)
 
-txt = st.text_area('Feedback', value="teste 1 \nteste 2 \n t t t\no")
+texto = feedback.loc[id, feedback]
+txt = st.text_area('Feedback', value=texto)
 
     
     
