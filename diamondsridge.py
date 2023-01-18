1fbb79d0d5f18b3e09a33214d2f9454a5a7c5ccd
edjaper <66147047+edjaper@users.noreply.github.com>
2023-01-17 22:29:55 -0300

    Update diamondsridge.py

--- diamondsridge.py
+++ diamondsridge.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from gsheetsdb import connect
 
 df = st.cache(pd.read_csv, ttl=300)('df_x_SKBfregression_545noADME_withYandYpredandId.csv', sep=',', decimal='.')
 feedback = st.cache(pd.read_csv)('feedback.csv', sep=';;#,;', decimal='.')
@@ -8,7 +9,17 @@ feedback = st.cache(pd.read_csv)('feedback.csv', sep=';;#,;', decimal='.')
 #if "feedback" not in st.session_state:
 #    st.session_state['feedback'] = pd.DataFrame(columns=['id','feedback'])
 
-
+# Create a connection object.
+conn = connect()
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+sheet_url = st.secrets["public_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 def onSave(a, b):
     df_feedback_save = feedback.copy()
@@ -35,3 +46,7 @@ if (texto=="nan"):
 txt = st.text_input('Feedback', value=texto)
 
 st.button("Salvar", on_click = onSave(id, txt))
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
